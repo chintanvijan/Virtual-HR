5bc425689bec5b3f6b4895277f60883b5721e231
Chintan Vijan <35058356+chintanvijan@users.noreply.github.com>
2018-12-20 22:01:44 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired,Length,Email
-class RegisterForm(FlaskForm):
-	fname = StringField('First Name',validators=[DataRequired(),Length(min=2,max=20)])
-	lname = StringField('Last Name',validators=[DataRequired(),Length(min=2,max=20)])
-	email = StringField('Email',validators=[DataRequired(),Email()])
-	sub = SubmitField('Register')
